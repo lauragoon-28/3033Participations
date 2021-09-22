b85a2e57ec4fb92c740ef4a7d65a833c7f1b7432
Laura Goon <laura.goon@ou.edu>
2021-09-22 16:08:12 -0500

    Finished the Participation

---
+++ Participations/Wpf-Contact_List/Contacts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wpf_Contact_List
+{
+    public class Contacts
+    {
+        public int id {get;set;}
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public string Email {get;set;}
+        public string URL { get; set; }
+
+        public Contacts()
+        {
+            id = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            URL = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+    }
+}
---
+++ Participations/Wpf-Contact_List/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Contact_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            string[] contents = File.ReadAllLines("contacts.txt");
+
+            for (int i = 1; i < contents.Length; i++)
+            {
+                string line = contents[i];
+
+                string[] pieces = line.Split("|");
+
+                string id = pieces[0];
+                string firstName = pieces[1];
+                string lastName = pieces[2];
+                string email = pieces[3];
+                string url = pieces[4];
+
+                Contacts c = new Contacts();
+                c.id = Convert.ToInt32(id);
+                c.FirstName = firstName;
+                c.LastName = lastName;
+                c.Email = email;
+                c.URL = url;
+
+                lstContacts.Items.Add(c);
+            }
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            Contacts selectedContact = (Contacts)lstContacts.SelectedItem;
+            txtFirstName.Text = selectedContact.FirstName;
+            txtLastName.Text = selectedContact.LastName;
+            txtEmail.Text = selectedContact.Email;
+            var uri = new Uri(selectedContact.URL);
+            var img = new BitmapImage(uri);
+            imgURL.Source = img;
+        }
+    }
+}
