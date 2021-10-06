51c91ea360db2151a556fca9e9afdc1e40486eb8
Laura Goon <laura.goon@ou.edu>
2021-10-06 16:16:31 -0500

    kinda finished the participation

---
+++ Participations/JSON - Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON___Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon").Result;
+
+                PokemonAPI results  = JsonConvert.DeserializeObject<PokemonAPI>(json);
+
+                foreach (var item in results.results)
+                {
+                    cboPokemon.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Result selectedItem = (Result)cboPokemon.SelectedItem;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon/pikachu").Result;
+
+                PokeAPI result = JsonConvert.DeserializeObject<PokeAPI>(json);
+
+                txtHeight.Text = Convert.ToString(result.height);
+                txtWeight.Text = Convert.ToString(result.weight);
+
+                var uri = new Uri(result.sprite.front_default);
+                var img = new BitmapImage(uri);
+                imgfront.Source = img;
+
+                //var uri = new Uri(result.sprite.back_default);
+                //var img = new BitmapImage(uri);
+                //imgBack.Source = img;
+            }
+
+        }
+    }
+}
---
+++ Participations/JSON - Pokemon/PokemonAPI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON___Pokemon
+{
+    public class PokemonAPI
+    {
+        public List<Result> results { get; set; }
+
+    }
+
+    public class PokeAPI
+    {
+        public int height { get; set; }
+        public int weight { get; set; }
+        public sprites sprite { get; set; }
+    }
+    public class sprites
+    {
+        public string front_default { get; set; }
+        public string back_default { get; set; }
+    }
+
+    public class Result
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
