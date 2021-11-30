28c0d75162e5b7c61f28bb3a93783322eb1bdd5b
lauragoon-28 <laura.goon@ou.edu>
2021-11-30 17:43:25 -0600

    hello

---
+++ Participations/Beginner/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Beginner.MvcApplication" Language="C#" %>
---
+++ Participations/MVC - Beginner/Controllers/PokemonController.cs
@@ -0,0 +1,43 @@
+﻿using MVC___Beginner.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC___Beginner.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            PokemonAPI allPokemon;
+
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100").Result;
+
+                allPokemon = JsonConvert.DeserializeObject<PokemonAPI>(json);
+            }
+
+            return View(allPokemon.results);
+        }
+
+        public ActionResult Info(string id)
+        {
+            PokemonInfo info;
+
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{id}").Result;
+
+                info = JsonConvert.DeserializeObject<PokemonInfo>(json);
+            }
+
+            return View(info);
+        }
+    }
+}
